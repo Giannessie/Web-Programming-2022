ebefffc818452f8a0e3c1cfca8c00f92de9be82e
Giannessie.19 <giannessiee@gmail.com>
2022-01-02 17:04:39 +0700

    Menambahkan Folder

---
+++ Praktikum 9/image picker.js
@@ -0,0 +1,47 @@
+// JavaScript Document
+function pilih(){
+    var pil=document.getElementById("form1").pic.value;
+     if (pil=="1")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/tom holland.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }
+       else if (pil=="2")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/zendaya.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }
+       else if (pil=="3")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/jacob batalon.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }
+       else if (pil=="4")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/marisa tomei.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }
+       else if (pil=="5")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/willem dafoe.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }
+       else if (pil=="6")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/alfred molina.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }
+       else if (pil=="7")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/thomas haden.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }
+       else if (pil=="8")
+       {
+           document.getElementById("img").innerHTML="<img src='IMG/jamie foxx.jpg'>";
+           window.alert("Ini Adalah Gambar Dari Pemeran Spider-man No Way Home.jpg");
+       }else{
+           document.getElementById("img").innerHTML="<img src='IMG/icon search.jpg'>";
+       }
+       
+   }
